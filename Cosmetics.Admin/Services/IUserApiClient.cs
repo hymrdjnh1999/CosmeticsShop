b46723468054b8c8f6ab5e99122060bb37dc79fd
Voi Be Nho <hymrdjnh199x9@gmail.com>
2021-03-20 19:37:23 +0700

    ds

---
+++ Cosmetics.Admin/Services/IUserApiClient.cs
@@ -0,0 +1,13 @@
+﻿using Cosmetics.ViewModels.Systems.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cosmetics.Admin.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Aithenticate(LoginRequest request);
+    }
+}
---
+++ Cosmetics.Admin/Services/UserApiClient.cs
@@ -0,0 +1,30 @@
+﻿using Cosmetics.ViewModels.Systems.Users;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetics.Admin.Services
+{
+    public class UserApiClient : IUserApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public UserApiClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<string> Aithenticate(LoginRequest request)
+        {
+            var json = JsonConvert.SerializeObject(request);
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri("https://localhost:5001");
+            var response = await client.PostAsync("/api/users/authenticate", httpContent);
+            var token = await response.Content.ReadAsStringAsync();
+            return token;
+        }
+    }
+}
