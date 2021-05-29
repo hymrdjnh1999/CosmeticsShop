d79aedf67c8f48947ab6e942036efa2e0336f52d
Voi Be Nho <hymrdjnh199x9@gmail.com>
2021-05-29 18:42:46 +0700

    display order detail

---
+++ Cosmetics.ViewModels/Catalogs/Orders/ClientCreateOrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Cosmetics.ViewModels.Catalogs.Carts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmetics.ViewModels.Catalogs.Orders
+{
+    public class ClientCreateOrderViewModel
+    {
+        public int OrderId { get; set; }
+        public string ClientName { get; set; }
+        public string ClientNote { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipPhone { get; set; }
+        public bool SaveShipInfo { get; set; }
+        public ClientCartViewModel ClientCart { get; set; }
+    }
+}
