f18415feb72bfa8c9d0d5f222131cfd9b5db9c3b
Voi Be Nho <hymrdjnh199x9@gmail.com>
2021-08-13 01:40:03 +0700

    test deploy

--- Cosmetics.WebAPI/Startup.cs
+++ Cosmetics.WebAPI/Startup.cs
@@ -158,7 +158,7 @@ namespace Cosmetics.WebAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseAuthentication();
